0cb152714f3a19dd2c629e1a92100514058d35f7
jalikins <jalikins05@gmail.com>
2026-07-11 07:52:22 -0400

    Fix RSS uppercase GET and pin sitemap version

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,8 @@ import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 
-export async function get(context) {
+// Changed 'get' to 'GET'
+export async function GET(context) {
   const blog = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
